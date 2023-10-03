5a62927339c4863d351c8ac70303a2fa7ce85299
virogg <itisinter@gmail.com>
2023-10-03 22:41:50 +0300

    Removed unused fields in Header structures.

--- bmp3/bitmap.h
+++ bmp3/bitmap.h
@@ -10,11 +10,7 @@
 #pragma pack(push, 1)
 struct BMPFileHeader {
     uint16_t file_type{0x4D42};
-    uint32_t file_size{0};
-    uint16_t reserved1{0};
-    uint16_t reserved2{0};
-    uint32_t offset_data{0};
-//    uint16_t unused[6]{ 0 };
+    uint16_t unused[6]{0};
 };
 
 struct BMPInfoHeader {
@@ -23,27 +19,26 @@ struct BMPInfoHeader {
     int32_t height{0};
     uint16_t planes{1};
     uint16_t bit_count{0};
-    uint32_t compression{0};
-    uint32_t size_image{0};
-    int32_t x_pixels_per_meter{0};
-    int32_t y_pixels_per_meter{0};
-    uint32_t colors_used{0};
-    uint32_t colors_important{0};
-//    uint32_t unused[16]{ 0 };
+    uint32_t unused[6]{0};
 };
 
 #pragma pack(pop)
+
 class BMP {
 private:
     BMPFileHeader file_header_;
     BMPInfoHeader bmp_info_header_;
-    char** dat_;
+    char **dat_;
 
 public:
     void read(const char *fname);
+
     void write(const char *fname);
+
     void rotate_right();
+
     void rotate_left();
+
     void apply_gaussian(int radius, float sigma);
 };
 
